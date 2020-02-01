8ec5780510d3ade853ad2ff338f931c76846b603
Daniel Greenidge <dev@danielgreenidge.com>
2020-02-01 11:24:56 -0500

    Add script to install terminfo on MacOS

--- bin/install_tmux_256color.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl -L http://invisible-island.net/datafiles/current/terminfo.src.gz \
-    -o /tmp/terminfo.src.gz
-cd /tmp
-gunzip terminfo.src.gz
-tic -x terminfo.src
-rm -rf terminfo.src
