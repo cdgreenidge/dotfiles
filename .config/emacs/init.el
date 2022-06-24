02b78e6514fb3223e619aedc5f0a390af2b2749f
Daniel Greenidge <dev@danielgreenidge.com>
2022-06-24 16:59:04 -0600

    Switch from straight to package.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,25 +2,9 @@
 ;; We have to do this before loading the literate config because otherwise
 ;; we will accidentally load an old, incomaptible version of org-mode
 
-(setq straight-recipes-gnu-elpa-use-mirror t)
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(setq package-enable-at-startup nil)
-(straight-use-package 'use-package)
-(straight-use-package 'org)  ; Keep straight from clobbering built-in org
-(setq straight-use-package-by-default t)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Load our config
 (org-babel-load-file "~/.config/emacs/config.org")
